8a54b71f7165b5c258e7b8c897253f54a923a6d8
Abhishek <abhishek@Abhisheks-MacBook-Air.local>
2026-07-19 09:29:10 +0530

    feat: state hooks complete

--- src/modules/hooksConcept/stateMgtHook/state.hook.tsx
+++ src/modules/hooksConcept/stateMgtHook/state.hook.tsx
@@ -4,7 +4,7 @@ import { useState } from "react";
 const StateMgmtHooks = () => {
   const [count, setCount] = useState<number>();
   return (
-    <div className="flex gap-3 ">
+    <div className="flex gap-3">
       <Button onClick={() => setCount((count ?? 0) + 1)}>{count}</Button>
     </div>
   );
